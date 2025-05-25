619fcf6d5c1e497bfbb13aaa69223af76bd9f556
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2025-05-25 22:47:30 +0500

    feat: new game logic

--- Assets/MyProject/Scripts/Monster/HardMonster/MonsterView.cs
+++ Assets/MyProject/Scripts/Monster/HardMonster/MonsterView.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HardMonsterView : MonoBehaviour
+public class MonsterView : MonoBehaviour
 {
     public readonly int Jumpscare = Animator.StringToHash(nameof(Jumpscare));
 
--- Assets/MyProject/Scripts/Monster/Monster.cs
+++ Assets/MyProject/Scripts/Monster/Monster.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public abstract class Monster : MonoBehaviour
+public class Monster : MonoBehaviour
 {
     [SerializeField] protected SoundPlayer _soundPlayer;
     [SerializeField] private AudioClip _screamSound;
@@ -25,32 +25,25 @@ public abstract class Monster : MonoBehaviour
         _bideCoroutine = StartCoroutine(Bide(_wardrobe.InteractWithDoor));
     }
 
+    private void OnEnable() =>
+        _wardrobe.DoorOpened += OnDoorOpened;
+
+    private void OnDisable() =>
+        _wardrobe.DoorOpened -= OnDoorOpened;
+
+    private void OnDoorOpened() =>
+        TryToJumpscare();
+
     public void SetAbleStatus(bool isChangingState) =>
         _isChangingState = isChangingState;
-
-    protected virtual IEnumerator WaitForChangeState(Action action)
-    {
-        yield return _wait;
-
-        action.Invoke();
-    }
 
     public void TryToJumpscare() => 
         _stateChangerCoroutine = StartCoroutine(WaitForChangeState(Jumpscare));
 
-    protected void Jumpscare()
-    {
-        StopCoroutine(_bideCoroutine);
-
-        Jumpedscare?.Invoke();
-
-        _soundPlayer.PlaySound(_screamSound);
-    }
-
-    public IEnumerator Bide(Action<bool> action)
+    private IEnumerator Bide(Action<bool> action)
     {
         while (enabled)
-        {             
+        {
             yield return new WaitForSeconds(UnityEngine.Random.Range(_monster.BideMinTime, _monster.BideMaxTime));
 
             action.Invoke(_isChangingState);
@@ -63,4 +56,20 @@ public abstract class Monster : MonoBehaviour
             action.Invoke(false);
         }
     }
+
+    private IEnumerator WaitForChangeState(Action action)
+    {
+        yield return _wait;
+
+        action.Invoke();
+    }
+
+    private void Jumpscare()
+    {
+        StopCoroutine(_bideCoroutine);
+
+        Jumpedscare?.Invoke();
+
+        _soundPlayer.PlaySound(_screamSound);
+    }    
 }
